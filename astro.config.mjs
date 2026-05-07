7ce89b4b15b9fc78fb23d04b0721d8f3d164aab2
Jeff LeBoeuf <jeff@jeff.how>
2026-05-07 13:58:27 -0400

    header done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import pagefind from 'astro-pagefind';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), pagefind()]
 });
